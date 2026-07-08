98dcd8fd1ce47545fbb605a850a0a99cfdab5575
officemohitjaiswal <officemohitjaiswal.bltr@gmial.com>
2026-07-09 01:19:57 +0530

    port fix

--- ShopManagementSystem.API/Program.cs
+++ ShopManagementSystem.API/Program.cs
@@ -10,18 +10,18 @@ using ShopManagementSystem.Infrastructure.Persistence.Seed;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// var port = Environment.GetEnvironmentVariable("PORT");
-// if (!string.IsNullOrWhiteSpace(port))
-// {
-//     builder.WebHost.UseUrls($"http://*:{port}");
-// }
+var port = Environment.GetEnvironmentVariable("PORT");
+if (!string.IsNullOrWhiteSpace(port))
+{
+    builder.WebHost.UseUrls($"http://*:{port}");
+}
 
-// Override connection string from environment variable if available (for Render deployment)
-// var connectionStringEnv = Environment.GetEnvironmentVariable("DATABASE_CONNECTION_STRING");
-// if (!string.IsNullOrWhiteSpace(connectionStringEnv))
-// {
-//     builder.Configuration["ConnectionStrings:DefaultConnection"] = connectionStringEnv;
-// }
+//DO NOT REMOVE Override connection string from environment variable if available (for Render deployment)
+var connectionStringEnv = Environment.GetEnvironmentVariable("DATABASE_CONNECTION_STRING");
+if (!string.IsNullOrWhiteSpace(connectionStringEnv))
+{
+    builder.Configuration["ConnectionStrings:DefaultConnection"] = connectionStringEnv;
+}
 
 builder.Services.AddApplication();
 builder.Services.AddInfrastructure(builder.Configuration);
